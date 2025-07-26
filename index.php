490f95ff4097c8f36f803ec3d4d8cd27f8a19d23
Adithep Kerdthong <adithep.kerdthong@gmail.com>
2025-07-26 02:24:29 +0000

    🔧 Auto commit: update project files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="th">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>แบบฟอร์มประกันภัย - บริษัท ฟอลคอนประกันภัย จำกัด (มหาชน)</title>
     <link rel="icon" href="assets/icon-white.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
